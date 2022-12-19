5c294d103033986e1201245bd87ec2545c62cf12
Haikotjo <haikowierda@hotmail.com>
2022-12-19 19:44:53 +0100

    oefenen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    const  [off, setOff] =  useState(true)
+    console.log(off)
   return (
-    <main className="off">
+
+    <main className={off === true ? "off" : "on"}>
       <section>
         <div className="dot"></div>
-        <button type="button">Turn on/off</button>
+        {off ? <h1>The button is on</h1> :
+            <h1>The button is off</h1>}
+        <button
+            disabled={off === false}
+            type="button"
+            onClick={() => setOff(false)}>
+          Turn on/off
+        </button>
       </section>
     </main>
   );
